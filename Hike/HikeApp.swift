d88fd672b7eeeaef79e03a1608dd02aee6f15110
Victor Lima <victor.limaeu@gmail.com>
2024-01-10 19:46:27 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Victor Lima on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
